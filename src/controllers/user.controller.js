d5f31ca033b4bfd44f404ead62676e51731b89ff
Mohsanul786 <mdmohsan2407@gmail.com>
2025-05-16 15:08:19 +0530

    Update user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({message:"Ok"})
+  // get the user details
+  // validation - not empty
+  // check if the user is already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary
+  // create user object
+  // remove password and refresh token field from response
+  // check for user creation
+  // save the details to database
 })
 
 export {registerUser}
